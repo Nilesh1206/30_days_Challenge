5058fff83140160fd4d79961cfa6d9d09680eda9
@Nilesh_Gopale <66791178+Nilesh1206@users.noreply.github.com>
2022-01-02 11:30:29 +0530

    Create 7_Position_of_Mth_Items.cpp

---
+++ 7_Position_of_Mth_Items.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+class Solution {
+  public:
+    int findPosition(int N , int M , int K) {
+        // code here
+        return ((M+K-2)%N+1);
+    }
+};
+// Time COmplexity :O(1)
+// Space Complexity :O(1)
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N,M,K;
+        
+        cin>>N>>M>>K;
+
+        Solution ob;
+        cout << ob.findPosition(N,M,K) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
